1965673c4478b6e07fc01cb091f17a8a57051e71
airwayzyuri <yuri@airwayz.co>
2024-05-01 17:17:22 +0300

    init commit

---
+++ apps/operators/protos/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+// #[derive(CustomResource, Debug, Serialize, Deserialize, Default, Clone, JsonSchema)]
+// #[kube(group = "kube.rs", version = "v1", kind = "Document", namespaced)]
+// pub struct DocumentSpec {
+//     title: String,
+//     content: String,
+// }
+// 
+// fn das() {
+//     let docs: Api<Document> = Api::default_namespaced(client);
+//     let d = Document::new("guide", DocumentSpec::default());
+//     println!("doc: {:?}", d);
+//     println!("crd: {:?}", serde_yaml::to_string(&Document::crd()));
+// }
